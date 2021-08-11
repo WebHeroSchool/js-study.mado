b3062752c12acd6c3334e17145e90ee651d5e883
Madina <mgabanova@mail.ru>
2021-08-11 11:13:08 +0300

    lesson 2

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('Hello');
+// console.log('Hello');
+
+const question1 = {
+  question,
+  answer,
+  currectAnswer
+}
+
+const question2 = {
+  question,
+  answer,
+  currectAnswer
+}
+
+const question3 = {
+  question,
+  answer,
+  currectAnswer
+}
+
+const question4 = {
+  question,
+  answer,
+  currectAnswer
+}
+
+let num,
+    score;
