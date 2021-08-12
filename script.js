6c3a684977c2b0fa5f7a4b926b6efe5b5591bf4f
Madina <mgabanova@mail.ru>
2021-08-12 17:20:10 +0300

    lesson 3

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-// console.log('Hello');
-
-const question1 = {
-  question,
-  answer,
-  currectAnswer
+let question1 = {
+  question: 'Какая игрушка была первой рекламируемой на телевидении?',
+  answer: ['a - Миссис Веселая Смешинка ','b - Мистер Картофельная Голова','c - Мисс Пушистая Шапочка','d - Чаки-Чаки'] ,
+  currectAnswer: 'b'
 }
 
-const question2 = {
-  question,
-  answer,
-  currectAnswer
+let question2 = {
+  question: 'В какой стране производится больше всего кофе в мире?',
+  answer: ['a - Индия','b - США','c - Бразилия ','d - Мексика'],
+  currectAnswer: 'c'
 }
 
-const question3 = {
-  question,
-  answer,
-  currectAnswer
+let question3 = {
+  question: 'Копрастастафобия - боязнь чего?',
+  answer: ['a - Кобр ','b - Запора','c - Устриц ','d - Башен'],
+  currectAnswer: 'b'
 }
 
-const question4 = {
-  question,
-  answer,
-  currectAnswer
+let question4 = {
+  question: 'Как еще называют День Благодарения?',
+  answer: ['a - День благодарности','b - Благодарственный день ','c - День индейки','d - Благодарность Индии '],
+  currectAnswer: 'c'
 }
 
 let num,
     score;
+let arrayQuest = [question1, question2, question3, question4];
+
+console.log(question4.answer);
